1893bf0110c36b367abc8498341213372692046d
Tejashri <tejashrik@globalnestsolutions.com>
2020-06-18 18:47:28 +0530

    banner interval change

--- application/views/frontend/default/home.php
+++ application/views/frontend/default/home.php
@@ -1,5 +1,5 @@
 <section>
-<div id="demo" class="carousel slide" data-ride="carousel" data-interval="false">
+<div id="demo" class="carousel slide" data-ride="carousel">
   <ul class="carousel-indicators">
     <li data-target="#demo" data-slide-to="0" class="active"></li>
     <li data-target="#demo" data-slide-to="1"></li>
