171c300d7d86be4d88a101b94c7ef7aea278ebf2
Pramod Pal <pramodp@globalnestsolutions.com>
2020-07-16 12:22:19 +0530

    Add time picker for lesson duration in admin and instructor

--- application/views/backend/admin/lesson_add.php
+++ application/views/backend/admin/lesson_add.php
@@ -97,11 +97,7 @@ $sections = $this->crud_model->get_section('course', $param2)->result_array();
 		
 		<div class="form-group">
 			<label><?php echo get_phrase('duration'); ?></label>
-			<div>
-				HH &nbsp;<input type="text" name="duration_hours" class="form-control" style="width:50px; display: inline;" placeholder="00">&nbsp;&nbsp;&nbsp;&nbsp;
-				MM &nbsp;<input type="text" name="duration_mins" class="form-control" style="width:50px; display: inline;" required placeholder="00">&nbsp;&nbsp;&nbsp;&nbsp;
-				SS &nbsp;<input type="text" name="duration_sec" class="form-control" style="width:50px; display: inline;" placeholder="00">
-			</div>
+			<input type="text" class="form-control" data-toggle='timepicker' data-minute-step="5" name="html5_duration" id = "html5_duration" data-show-meridian="false" value="00:00:00">
 		</div>
 	
     </div>
