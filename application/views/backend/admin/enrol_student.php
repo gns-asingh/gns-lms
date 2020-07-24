f38e4056ee545b4d0e8f54a639fc16c014d30c22
Divya-Deshmukh <Divyad@globalnestsolutions.com>
2020-07-24 15:16:11 +0530

    enrol student problem solved for admin

--- application/views/backend/admin/enrol_student.php
+++ application/views/backend/admin/enrol_student.php
@@ -25,7 +25,7 @@
 
                                 <div class="form-group">
                                     <label for="user_id"><?php echo get_phrase('user'); ?><span class="required">*</span> </label>
-                                    <select class="form-control  name="user_id[]" id="user_id" required multiple>
+                                    <select class="form-control"  name="user_id[]" id="user_id" required multiple>
                                         <!-- <option value=""><?php echo get_phrase('select_a_user'); ?></option> -->
                                         <?php $user_list = $this->user_model->get_user()->result_array();
                                             foreach ($user_list as $user):?>
